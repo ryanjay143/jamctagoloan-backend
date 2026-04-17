4e8da36f9eb011d0b48950e5e4de913fab80c3c7
Ryan Reyes <ryanjaytagolimotreyes@gmail.com>
2026-04-17 10:11:27 +0800

    t

--- routes/api.php
+++ routes/api.php
@@ -6,23 +6,19 @@ use App\Http\Controllers\ListOfMemberController;
 use App\Http\Controllers\EditMemberController;
 use App\Http\Controllers\TithesController;
 use App\Http\Controllers\Api\PlaylistController;
-use App\Http\Controllers\Api\ObsSyncController;
+use App\Http\Controllers\Api\ObsSyncController; // Siguroa nga naa kini nga import
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-
-    
 });
-
 
 Route::apiResource('list-of-member', ListOfMemberController::class);
 Route::apiResource('tithes', TithesController::class);
 Route::post('expenses', [TithesController::class, 'create']);
-
 Route::post('edit-member/{id}', [EditMemberController::class, 'edit']);
 
 Route::get('/playlists', [PlaylistController::class, 'index']);
 Route::post('/playlists/sync', [PlaylistController::class, 'sync']);
 Route::post('/playlists/upload', [PlaylistController::class, 'upload']);
-
+// Move this to api.php kay gamiton sa backend
 Route::post('/obs/update', [ObsSyncController::class, 'update']);
